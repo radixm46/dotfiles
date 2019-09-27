d74308e9f878dc9b739724d3bc80caca6b7ebf61
radixm46 <radixm46@gmail.com>
2019-09-27 15:27:57 +0900

    modify symlink

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-#RED = '\033[0;31m'
-#NC = '\033[0m'
-# DOT_FILES=(.vimrc .vim .tmux.conf .zshrc .zshenv .tmux)
-#
-#for file in ${DOT_FILES[@]}
-#do
-#    ln -s $HOME/dotfiles/$file $HOME/$file
-#done
+#!/bin/sh
 
  DOT_FILES=(.tmux.conf .zshrc .zshenv)
 
@@ -21,27 +12,8 @@ ln -s $HOME/dotfiles/.tmux $HOME/.tmux
 if [ -d "$HOME/.config" ]; then
     mkdir $HOME/.config
 fi
-ln -s $HOME/dotfiles/nvim/ $HOME/.config/nvim
 
-#setup for vim
-ln -s $HOME/dotfiles/nvim/ $HOME/.vim
+ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/nvim $HOME/.vim
 ln -s $HOME/dotfiles/nvim/init.vim $HOME/.vimrc
-
-
-#Check Dein
-#if [ -e ~/.vim/dein/repos/github.com/Shougo/dein.vim ]; then
-#    #echo -e "${RED}dein installed!!!${NC}"
-#    echo "dein installed!!!"
-#else
-#    echo "dein not installed!!"
-#    #gitクローンでdeinインストールを行う
-#    mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
-#    git clone https://github.com/Shougo/dein.vim.git \
-#            ~/.vim/dein/respos/github.com/Shougo/dein.vim
-#fi
-
-
-#todo
-#dein識別、インストールをvimrc(init.vim)から
-#zsh、tmux、vimの自動インストールなど
-#OS毎にセットアップファイルを切り替える
+ln -s $HOME/dotfiles/alacritty $HOME/.config/alacritty
