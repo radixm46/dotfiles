654c71899cf310493c029e1ecf5533b106b2372a
radixm46 <radixm46@gmail.com>
2019-10-01 15:34:27 +0900

    small improvements

--- setup.sh
+++ setup.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
- DOT_FILES=(.tmux.conf .zshrc .zshenv)
+ DOT_FILES=(.tmux.conf .tmux .zshrc .zshenv)
+ DOT_CONFIG=(nvim alacritty)
 
 for file in ${DOT_FILES[@]}
 do
-    if [ ! -f "$HOME/$file" ]; then
+    if [ ! -e "$HOME/$file" ]; then
         ln -s $HOME/dotfiles/$file $HOME/$file
     fi
 done
-ln -s $HOME/dotfiles/.tmux $HOME/.tmux
 
 #setup for nvim
 if [ ! -d "$HOME/.config" ]; then
     mkdir $HOME/.config
 fi
 
+for file in ${DOT_FILES[@]}
+do
+    if [ ! -e "$HOME/$file" ]; then
+        ln -s $HOME/dotfiles/$file $HOME/.config/$file
+    fi
+done
+
 # install vim config
-ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/dotfiles/nvim $HOME/.vim
 ln -s $HOME/dotfiles/nvim/init.vim $HOME/.vimrc
-ln -s $HOME/dotfiles/alacritty $HOME/.config/alacritty
