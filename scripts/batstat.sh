9201684f3d6172d7841801156e0e6521736c2c45
radixm46 <radixm46@gmail.com>
2019-10-07 10:18:49 +0900

    check os type

--- scripts/batstat.sh
+++ scripts/batstat.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -e
+case ${OSTYPE} in
+    darwin*)
+        battery_capacity='unsupported '
+        battery_charging=" "
+        ;;
+    linux*)
+        battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+        if [ $(</sys/class/power_supply/BAT0/status) = 'Charging' ]
+        then
+            battery_charging="\U26A1"
+            #battery_charging="\U1f50C"
+        else
+            battery_charging="\U1F50B"
+        fi
+        ;;
+esac
 
-battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-if [ $(</sys/class/power_supply/BAT0/status) = 'Charging' ]
-then
-    battery_charging="\U26A1"
-    #battery_charging="\U1f50C"
-else
-    battery_charging="\U1F50B"
-fi
 
 echo -e ${battery_capacity}'%'${battery_charging}
--- scripts/memstat.sh
+++ scripts/memstat.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # display current memory in GB
+case ${OSTYPE} in
+    darwin*)
+        mem_val='unsupported'
+        ;;
+    linux*)
+        mem_val=$(free -m | awk 'NR==2{printf("%.1fG (%.1f%)", $3/1024, $3/$2*100)}')
+        ;;
+esac
 
-mem_val=$(free -m | awk 'NR==2{printf("%.1fG (%.1f%)", $3/1024, $3/$2*100)}')
 
-echo -e ${mem_val}
+echo ${mem_val}
