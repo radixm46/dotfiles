cf4ef25aeb62fd0b4dd1854138954b116d37e1a6
radixm46 <radixm46@gmail.com>
2019-10-11 01:00:25 +0900

    support macbook

--- scripts/batstat.sh
+++ scripts/batstat.sh
@@ -6,14 +6,14 @@ case ${OSTYPE} in
         awk 'NR==2{print($3, $4)}' |\
         sed -E \
         -e 's/(\;| )//g' \
-        -e 's/(charged|charging)/'⚡'/'\
+        -e 's/charging/'⚡'/'\
+        -e 's/charged/'✔'/'\
         -e 's/discharging/'🔋'/'
         ;;
     linux*)
         if [ $(</sys/class/power_supply/BAT0/status) = 'Charging' ]
         then
             battery_charging="\U26A1"
-            #battery_charging="\U1f50C"
         else
             battery_charging="\U1F50B"
         fi
