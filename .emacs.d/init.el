4fee97264d0354c5bd2dbf91865f513585bc969f
radixm46 <radixm46@gmail.com>
2020-02-21 16:47:24 +0900

    add few lines for org-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,10 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
-;; emacs minimal configuration by radixm@9n4
+;; emacs minimal configuration by radixM491VA
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(set-default 'buffer-file-coding-system 'utf-8)
 (global-display-line-numbers-mode)
 (ido-mode t)
 
@@ -16,7 +19,8 @@
 (package-initialize)
 (setq package-archives
             '(("gnu" . "http://elpa.gnu.org/packages/")
-                      ("melpa" . "http://melpa.org/packages/")))
+              ("melpa" . "http://melpa.org/packages/")
+              ("org" . "http://orgmode.org/elpa/")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,3 +33,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; org-mode config
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
