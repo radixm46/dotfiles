40ce216a8a245b94a0b235dac6f46ba2d3978ab2
radixm46 <radixm46@gmail.com>
2020-03-01 09:26:11 +0900

    load by default package plugin

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,31 +8,100 @@
 (prefer-coding-system 'utf-8)
 (set-default 'buffer-file-coding-system 'utf-8)
 (global-display-line-numbers-mode)
+(column-number-mode t)
 (ido-mode t)
+(global-hl-line-mode t)
+(show-paren-mode t)
+(setq ring-bell-function 'ignore)
 
 
+;; log
+(setq message-log-maxa 10000)
+(setq history-length 1000)
+(setq history-delete-duplicates t)
 
-;; (load-theme 'solarized-dark t)
+;; locale and encoding
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq default-coding-systems 'utf-8)
+(setq coding-system-for-read 'utf-8)
+(setq coding-system-for-write 'utf-8)
+
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+(which-function-mode 1)
+(transient-mark-mode 1)
+
+
+;; init package
+(require 'package)
+
+(setq package-archives (append
+   '(
+     ("melpa-stable" . "https://stable.melpa.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("org" . "http://orgmode.org/elpa/")
+     )
+   package-archives
+   ))
+
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;;(set-input-method "japanese-skk")
-(package-initialize)
-(setq package-archives
-            '(("gnu" . "http://elpa.gnu.org/packages/")
-              ("melpa" . "http://melpa.org/packages/")
-              ("org" . "http://orgmode.org/elpa/")))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (solarized-theme ddskk))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+
+(setq load-prefer-newer t)
+
+(defvar required-packages
+  '(
+    ddskk
+    evil linum-relative
+    doom-modeline
+    neotree
+
+    ;; git
+    magit git-gutter
+    ))
+
+
+(dolist (pkg required-packages)
+  (unless (package-installed-p pkg)  ;; can't notify upgradable package
+    ;; (package-install pkg)
+    ;; (package-initialize)
+))
+
+;; skk config
+(when (require 'skk nil t)
+  (setq default-input-method "japanese-skk")
+  (require 'skk-study)
+)
+
+;; enable evil
+(when (require 'evil nil t)
+  (evil-mode 1)
+)
+
+
+;; enable relative line number
+(when (require 'linum-relative nil t)
+ (linum-relative-global-mode)
  )
+
+
+;; enable doom-modeline
+(when (require 'doom-modeline nil t)
+  (doom-modeline-mode 1)
+)
+
 
 ;; org-mode config
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
@@ -40,3 +109,19 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (ddskk))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
