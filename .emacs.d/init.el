0bf58333927a7eed5226043db2cf4b200c00d06e
radixm46 <radixm46@gmail.com>
2020-03-03 09:24:04 +0900

    add lsp plugins

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,6 +20,8 @@
     '(font . "HackGen Console for Powerline-16"))
   default-frame-alist))
 
+(tool-bar-mode -1)
+
 
 ;; log
 (setq message-log-maxa 10000)
@@ -112,6 +114,9 @@
   :config (linum-relative-global-mode)
 )
 
+(use-package all-the-icons
+  :ensure t
+) ; need installation by all-the-icons-color-install-fonts
 
 ;; enable doom-modeline
 (use-package doom-modeline
@@ -124,6 +129,11 @@
 
   ;; How wide the mode-line bar should be. It's only respected in GUI.
   (setq doom-modeline-bar-width 3)
+
+  ;; Whether display icons in the mode-line. Respects `all-the-icons-color-icons'.
+  ;; While using the server mode in GUI, should set the value explicitly.
+  ;(setq doom-modeline-icon (display-graphic-p))
+  (setq doom-modeline-icon t)
 
   ;; The limit of the window width.
   ;; If `window-width' is smaller than the limit, some information won't be displayed.
@@ -140,10 +150,6 @@
 
   (setq doom-modeline-buffer-file-name-style 'auto)
 
-  ;; Whether display icons in the mode-line. Respects `all-the-icons-color-icons'.
-  ;; While using the server mode in GUI, should set the value explicitly.
-  ;(setq doom-modeline-icon (display-graphic-p))
-  (setq doom-modeline-icon t)
 
   (setq doom-modeline-major-mode-icon t)
   (setq doom-modeline-major-mode-color-icon t)
@@ -242,8 +248,9 @@
 
   ;(global-hl-line-mode t)
   (doom-themes-visual-bell-config)
-  (doom-themes-neotree-config)
   (doom-themes-org-config)
+  (doom-themes-treemacs-config)
+  ;(doom-themes-neotree-config)
 )
 
 ;(use-package col-highlight
@@ -264,12 +271,105 @@
   (setq highlight-indent-guides-delay 0)
 )
 
-(use-package neotree
+;(use-package neotree
+;  :ensure t
+;  :config
+;  (global-set-key [f7] 'neotree-toggle)
+;  (setq neo-window-fixed-size nil)
+;)
+
+
+(use-package treemacs
   :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (global-set-key [f7] 'neotree-toggle)
-  (setq neo-window-fixed-size nil)
-)
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-width                         35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  ;:bind
+  ;(:map global-map
+  ;      ("M-0"       . treemacs-select-window)
+  ;      ("C-x t 1"   . treemacs-delete-other-windows)
+  ;      ("C-x t t"   . treemacs)
+  ;      ("C-x t B"   . treemacs-bookmark)
+  ;      ("C-x t C-t" . treemacs-find-file)
+  ;      ("C-x t M-t" . treemacs-find-tag)
+  ;      )
+  (define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action)
+  )
+
+(use-package treemacs-evil
+  :after treemacs evil
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(use-package treemacs-persp
+  :after treemacs persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
 
 
 (use-package magit
@@ -278,6 +378,86 @@
 
 (use-package git-gutter
   :ensure t)
+
+
+;; -------- package for lsp --------
+
+(use-package lsp-mode
+  :ensure t
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l", "s-l" )
+  :init (setq lsp-keymap-prefix "C-c C-l")
+   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         ;(XXX-mode . lsp)
+         ; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration)
+        )
+  :commands lsp
+ )
+
+;; optionally
+(use-package company :ensure t) ; completion
+(use-package company-lsp :ensure t :commands company-lsp)
+;; if you are helm user
+;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs
+  :ensure t
+  :commands lsp-treemacs-errors-list
+)
+
+;; optionally if you want to use debugger
+;(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+             :ensure t
+             :config
+             (which-key-mode))
+
+(use-package lsp-ui
+  :ensure t
+  :after lsp-mode
+  :custom (scroll-margin 0)
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+
+  (setq lsp-ui-doc-enable t)
+  (setq lsp-ui-doc-header t)
+  ;(setq lsp-ui-doc-include-signature t)
+  (setq lsp-ui-doc-max-width 150)
+  (setq lsp-ui-doc-max-height 30)
+  (setq lsp-ui-peek-enable t)
+)
+
+
+; -------- lauguage specific package --------
+(use-package rustic
+  :ensure t
+  :defer t
+ ;:init
+ ;(add-hook ‘rustic-mode-hook
+ ;‘(lambda ()
+ ;  (racer-mode t)
+ ;  (dumb-jump-mode t)
+ ;  (highlight-symbol-mode t)
+ ;  (rainbow-delimiters-mode t)
+ ;  (smartparens-mode t)))
+ ;:mode (“\\.rs$” . rustic-mode)
+ ;:commands (rustic-mode)
+
+ ;:config
+ ;(use-package quickrun
+ ;:defer t
+ ;:ensure t)
+ ;(use-package racer
+ ;:defer t
+ ;:ensure t)
+)
+
 
 
 ;; org-mode config
