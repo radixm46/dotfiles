066c6fbe64e90ba1ed5c0363f998582e6484a795
radixm46 <radixm46@gmail.com>
2020-03-03 20:35:11 +0900

    config lsp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -249,8 +249,8 @@
   ;(global-hl-line-mode t)
   (doom-themes-visual-bell-config)
   (doom-themes-org-config)
-  (doom-themes-treemacs-config)
-  ;(doom-themes-neotree-config)
+  ;(doom-themes-treemacs-config)
+  (doom-themes-neotree-config)
 )
 
 ;(use-package col-highlight
@@ -271,105 +271,21 @@
   (setq highlight-indent-guides-delay 0)
 )
 
-;(use-package neotree
-;  :ensure t
-;  :config
-;  (global-set-key [f7] 'neotree-toggle)
-;  (setq neo-window-fixed-size nil)
-;)
-
-
-(use-package treemacs
+(use-package neotree
   :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (progn
-    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-directory-name-transformer    #'identity
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-extension-regex          treemacs-last-period-regex-value
-          treemacs-file-follow-delay             0.2
-          treemacs-file-name-transformer         #'identity
-          treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   2
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-missing-project-action        'ask
-          treemacs-no-png-images                 nil
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        nil
-          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                      'left
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    nil
-          treemacs-recenter-after-tag-follow     nil
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   nil
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-asc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              1.5
-          treemacs-width                         35)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
-  ;:bind
-  ;(:map global-map
-  ;      ("M-0"       . treemacs-select-window)
-  ;      ("C-x t 1"   . treemacs-delete-other-windows)
-  ;      ("C-x t t"   . treemacs)
-  ;      ("C-x t B"   . treemacs-bookmark)
-  ;      ("C-x t C-t" . treemacs-find-file)
-  ;      ("C-x t M-t" . treemacs-find-tag)
-  ;      )
-  (define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action)
-  )
-
-(use-package treemacs-evil
-  :after treemacs evil
-  :ensure t)
-
-(use-package treemacs-projectile
-  :after treemacs projectile
-  :ensure t)
-
-(use-package treemacs-icons-dired
-  :after treemacs dired
-  :ensure t
-  :config (treemacs-icons-dired-mode))
-
-(use-package treemacs-magit
-  :after treemacs magit
-  :ensure t)
-
-(use-package treemacs-persp
-  :after treemacs persp-mode
-  :ensure t
-  :config (treemacs-set-scope-type 'Perspectives))
+  (global-set-key [f7] 'neotree-toggle)
+  (setq neo-window-fixed-size nil)
+  (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+  (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+)
 
 
 (use-package magit
@@ -380,18 +296,83 @@
   :ensure t)
 
 
+(use-package flycheck
+             :ensure t)
+
 ;; -------- package for lsp --------
 
 (use-package lsp-mode
   :ensure t
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l", "s-l" )
-  :init (setq lsp-keymap-prefix "C-c C-l")
-   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+  ; :init (setq lsp-keymap-prefix "C-c C-l")
+  :custom
+  (lsp-document-sync-method 'incremental) ;; always send incremental document
+  (lsp-response-timeout 5)
+  (lsp-prefer-flymake 'flymake)
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          ;(XXX-mode . lsp)
          ; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration)
         )
   :commands lsp
+  :bind (:map lsp-mode-map
+        ("C-c r"   . lsp-rename))
+  :config
+  (require 'lsp-clients)
+  ;; LSP UI tools
+  (use-package lsp-ui
+    :ensure t
+    :custom
+    ;; lsp-ui-doc
+    (lsp-ui-doc-enable t)
+    (lsp-ui-doc-header t)
+    (lsp-ui-doc-include-signature t)
+    (lsp-ui-doc-position 'top) ;; top, bottom, or at-point
+    (lsp-ui-doc-max-width 150)
+    (lsp-ui-doc-max-height 30)
+    (lsp-ui-doc-use-childframe t)
+    (lsp-ui-doc-use-webkit t)
+    ;; lsp-ui-flycheck
+    (lsp-ui-flycheck-enable nil)
+    ;; lsp-ui-sideline
+    (lsp-ui-sideline-enable nil)
+    (lsp-ui-sideline-ignore-duplicate t)
+    (lsp-ui-sideline-show-symbol t)
+    (lsp-ui-sideline-show-hover t)
+    (lsp-ui-sideline-show-diagnostics t) ;nil
+    (lsp-ui-sideline-show-code-actions t) ;nil
+    ;; lsp-ui-imenu
+    (lsp-ui-imenu-enable nil)
+    (lsp-ui-imenu-kind-position 'top)
+    ;; lsp-ui-peek
+    (lsp-ui-peek-enable t)
+    (lsp-ui-peek-peek-height 20)
+    (lsp-ui-peek-list-width 50)
+    (lsp-ui-peek-fontify 'on-demand) ;; never, on-demand, or always
+    :preface
+    (defun ladicle/toggle-lsp-ui-doc ()
+      (interactive)
+      (if lsp-ui-doc-mode
+        (progn
+          (lsp-ui-doc-mode -1)
+          (lsp-ui-doc--hide-frame))
+         (lsp-ui-doc-mode 1)))
+    :bind
+    (:map lsp-mode-map
+    ("C-c C-r" . lsp-ui-peek-find-references)
+    ("C-c C-j" . lsp-ui-peek-find-definitions)
+    ("C-c i"   . lsp-ui-peek-find-implementation)
+    ("C-c m"   . lsp-ui-imenu)
+    ("C-c s"   . lsp-ui-sideline-mode)
+    ("C-c d"   . ladicle/toggle-lsp-ui-doc))
+    :hook
+    (lsp-mode . lsp-ui-mode))
+  ;; Lsp completion
+  (use-package company-lsp
+    :custom
+    (company-lsp-cache-candidates t) ;; always using cache
+    (company-lsp-async t)
+    (company-lsp-enable-recompletion nil))
  )
 
 ;; optionally
@@ -401,10 +382,6 @@
 ;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
 ;; if you are ivy user
 ;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs
-  :ensure t
-  :commands lsp-treemacs-errors-list
-)
 
 ;; optionally if you want to use debugger
 ;(use-package dap-mode)
@@ -415,23 +392,6 @@
              :ensure t
              :config
              (which-key-mode))
-
-(use-package lsp-ui
-  :ensure t
-  :after lsp-mode
-  :custom (scroll-margin 0)
-  :hook (lsp-mode . lsp-ui-mode)
-  :config
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-
-  (setq lsp-ui-doc-enable t)
-  (setq lsp-ui-doc-header t)
-  ;(setq lsp-ui-doc-include-signature t)
-  (setq lsp-ui-doc-max-width 150)
-  (setq lsp-ui-doc-max-height 30)
-  (setq lsp-ui-peek-enable t)
-)
 
 
 ; -------- lauguage specific package --------
