a5e69750f50017930ae6d61fbfc8f158d8b2ad68
radixm46 <radixm46@gmail.com>
2020-06-26 17:17:36 +0900

    Revert "change free mem val"
    This reverts commit 4e120a61e76bf8566920f7bbaeb27551b1d3b823.

--- bin/memstat.sh
+++ bin/memstat.sh
@@ -6,7 +6,7 @@ case $(uname) in
         mem_val="is \UF179"
         ;;
     Linux*)
-        mem_val=$(free -m | awk 'NR==2{printf("%.1d% (%.1fG)", ($3+$6)/$2*100, ($3+$6)/1024)}')
+        mem_val=$(free -m | awk 'NR==2{printf("%.1d% (%.1fG)", $3/$2*100, $3/1024)}')
         ;;
     *)
         mem_val="\UF00D"
