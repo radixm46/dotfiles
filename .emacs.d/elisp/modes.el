9119698b5096f4bc6281c7601cc45b3084c1cee4
radixm46 <radixm46@gmail.com>
2020-06-30 12:44:00 +0900

    add pipenv package

--- .emacs.d/elisp/modes.el
+++ .emacs.d/elisp/modes.el
@@ -78,7 +78,17 @@
   :ensure t
   :hook (python-mode . lsp)
   :mode ("\\.py\\'" . python-mode)
-)
+  )
+
+(use-package pipenv
+  :ensure t
+  :hook (python-mode . pipenv-mode)
+  :init
+  (setq
+   pipenv-projectile-after-switch-function
+   #'pipenv-projectile-after-switch-extended))
+
+
 
 
 (use-package go-mode
