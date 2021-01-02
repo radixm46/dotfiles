93212b6cf5c1bef7c9f513d47171f40eedbab45b
radixm46 <radixm46@gmail.com>
2021-01-02 23:04:31 +0900

    display macOS memory pressure

--- bin/memstat.sh
+++ bin/memstat.sh
@@ -3,7 +3,7 @@
 nerdmem="\UF85A"
 case $(uname) in
     Darwin)
-        mem_val="is \UF179"
+        mem_val=$(memory_pressure | awk 'NR==28{ print $5 "\% (prs)" }')
         ;;
     Linux*)
         mem_val=$(free -m | awk 'NR==2{printf("%.1d% (%.1fG)", $3/$2*100, $3/1024)}')
