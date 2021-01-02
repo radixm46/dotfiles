dbdeb5495559191bf4101426454e56910327ab79
radixm46 <radixm46@gmail.com>
2021-01-03 00:43:53 +0900

    replace if statements

--- bin/batstat.sh
+++ bin/batstat.sh
@@ -2,40 +2,28 @@
 
 # print charging battery sign from nerd fonts
 function print_chrg_batt {
-    # nerd fonts charging bat stat array (20, 30, 40, 60, 80, 90, 100)
-    charging_bat_arr=('\UF585' '\UF586' '\UF587' '\UF588' '\UF589' '\UF58A' \
-                               '\UF584' '\UF578' '\UF582')
-    if ((0 < "$1")) && (("$1" <= 20)); then bat_idx=0
-    elif ((20 < "$1")) && (("$1" <= 30)); then bat_idx=1
-    elif ((30 < "$1")) && (("$1" <= 40)); then bat_idx=2
-    elif ((40 < "$1")) && (("$1" <= 60)); then bat_idx=3
-    elif ((60 < "$1")) && (("$1" <= 80)); then bat_idx=4
-    elif ((80 < "$1")) && (("$1" <= 90)); then bat_idx=5
-    elif ((90 < "$1")) && (("$1" < 100)); then bat_idx=6
-    elif ((100 == "$1")); then bat_idx=7
-    else bat_idx=8
+    # nerd fonts charging bat stat array (20, 20, 20, 30, 40, 40, 60, 80, 90, 100)
+    charging_bat_arr=('\UF585' '\UF585' '\UF585' '\UF586' '\UF587' '\UF587' \
+                               '\UF588' '\UF589' '\UF58A'  '\UF584' '\UF578')
+    bat_idx=$(expr $1 / 10)
+    if ((0 <= ${bat_idx})) && ((${bat_idx} <= 10)); then
+        printf ${non_charging_bat_arr[bat_idx]}
+    else
+        printf '\UF582'
     fi
-    printf ${charging_bat_arr[bat_idx]}
 }
 
 # print non charging battery sign from nerd fonts
-function print_non_chrg_batt {
-    # nerd fonts non charging bat stat array (10 to 100)
-    non_charging_bat_arr=('\UF579' '\UF57A' '\UF57B' '\UF57C' '\UF57D' '\UF57E' \
-                                   '\UF57F' '\UF580' '\UF581' '\UF578' '\UF582')
-    if ((0 < "$1")) && (("$1" <= 10)); then bat_idx=0;
-    elif ((10 < "$1")) && (("$1" <= 20)); then bat_idx=1
-    elif ((20 < "$1")) && (("$1" <= 30)); then bat_idx=2
-    elif ((30 < "$1")) && (("$1" <= 40)); then bat_idx=3
-    elif ((50 < "$1")) && (("$1" <= 60)); then bat_idx=4
-    elif ((60 < "$1")) && (("$1" <= 70)); then bat_idx=5
-    elif ((70 < "$1")) && (("$1" <= 80)); then bat_idx=6
-    elif ((80 < "$1")) && (("$1" <= 90)); then bat_idx=7
-    elif ((90 < "$1")) && (("$1" < 100)); then bat_idx=8
-    elif ((100 == "$1")); then bat_idx=9
-    else bat_idx=10
+function print_non_chrg_batt() {
+    # nerd fonts non charging bat stat array (00 to 100)
+    non_charging_bat_arr=('\UF58D' '\UF579' '\UF57A' '\UF57B' '\UF57C' '\UF57D' \
+                                   '\UF57E' '\UF57F' '\UF580' '\UF581' '\UF578')
+    bat_idx=$(expr $1 / 10)
+    if ((0 <= ${bat_idx})) && ((${bat_idx} <= 10)); then
+        printf ${non_charging_bat_arr[bat_idx]}
+    else
+        printf '\UF582'
     fi
-    printf ${non_charging_bat_arr[bat_idx]}
 }
 
 pwr_conn='\UFBA3'
