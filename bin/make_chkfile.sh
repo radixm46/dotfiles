61e9805c67a694f0ae515a703c451a2231d05c0b
radixm46 <radixm46@gmail.com>
2021-01-07 01:48:17 +0900

    improved on darwin

--- bin/make_chkfile.sh
+++ bin/make_chkfile.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-printf '> Checking... '${1}'\n'; \
-if [ ! -e ${1}  ]; then \
-    printf '==> NOT EXIST\n'; \
-elif file -b ${1} | grep -q 'symbolic link'; then \
-    printf "==> $(file -b ${1})\n"; \
-elif file -b ${1} | grep -q 'directory\|file'; then \
-    printf '==> FILE or DIRECTORY\n'; \
+case "$(uname)" in
+    Darwin) fcopt='bh' ;;
+    Linux*) fcopt='b' ;;
+    *) fcopt='b' ;;
+esac
+
+printf '> Checking... '${1}'\n';
+if [ ! -e ${1}  ]; then
+    printf '==> NOT EXIST\n';
+elif file -${fcopt} ${1} | grep -q 'symbolic link'; then
+    printf "==> $(file -${fcopt} ${1})\n";
+elif file -${fcopt} ${1} | grep -q 'directory\|file'; then
+    printf '==> FILE or DIRECTORY\n';
 else
-    printf '==> NOT VALID\n'; \
+    printf '==> NOT VALID\n';
 fi
