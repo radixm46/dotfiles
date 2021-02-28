9a1c59ffb97440f0b617b213c6891cea59e20d92
radixm46 <radixm46@gmail.com>
2021-02-28 21:48:56 +0900

    configure multiple variables via single setq function
    change indent

--- .emacs.d/elisp/initskk.el
+++ .emacs.d/elisp/initskk.el
@@ -1,24 +1,19 @@
 ;; skk init file
-
-(setq skk-use-azik t)
-(setq skk-azik-keyboard-type 'us101)
-(setq skk-show-inline t)
-(setq skk-show-tooltip t)
-(setq skk-show-candidates-always-pop-to-buffer t)
-(setq skk-henkan-show-candidates-rows 2)
-
-;(setq skk-dcomp-activate t)
-;(setq skk-dcomp-multiple-activate t)
-;(setq skk-dcomp-multiple-rows 10)
+(setq skk-use-azik t
+      skk-azik-keyboard-type 'us101
+      skk-show-inline t
+      skk-show-tooltip t
+      skk-show-candidates-always-pop-to-buffer t
+      skk-henkan-show-candidates-rows 2
+      skk-dcomp-activate nil
+      ;skk-dcomp-multiple-activate t
+      ;skk-dcomp-multiple-rows 10
+      skk-japanese-message-and-error t
+      skk-show-japanese-menu t
+      skk-delete-implies-kakutei nil
+      skk-use-look t
+      skk-henkan-strict-okuri-precedence t)
 ;(define-key skk-j-mode-map (kbd "M-n"))
-
-(setq skk-japanese-message-and-error t)
-(setq skk-show-japanese-menu t)
-
-(setq skk-delete-implies-kakutei nil)
-(setq skk-use-look t)
-(setq skk-henkan-strict-okuri-precedence t)
-
 
 (if (file-directory-p skk-get-jisyo-directory)
     (ignore) (skk-get skk-get-jisyo-directory))
