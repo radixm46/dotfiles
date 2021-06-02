03b654517608e066a2daf93e75d45a618ef9faa7
radixm46 <radixm46@gmail.com>
2021-06-02 17:13:33 +0900

    update style of custom variables

--- .emacs.d/elisp/modes.el
+++ .emacs.d/elisp/modes.el
@@ -30,47 +30,47 @@
         ("C-x t B"   . treemacs-bookmark)
         ("C-x t C-t" . treemacs-find-file)
         ("C-x t M-t" . treemacs-find-tag))
+  :custom
+  (treemacs-deferred-git-apply-delay      0.5)
+  (treemacs-directory-name-transformer    #'identity)
+  (treemacs-display-in-side-window        t)
+  (treemacs-eldoc-display                 t)
+  (treemacs-file-event-delay              5000)
+  (treemacs-file-extension-regex          treemacs-last-period-regex-value)
+  (treemacs-file-follow-delay             0.2)
+  (treemacs-file-name-transformer         #'identity)
+  (treemacs-follow-after-init             t)
+  (treemacs-git-command-pipe              "")
+  (treemacs-goto-tag-strategy             'refetch-index)
+  (treemacs-indentation                   2)
+  (treemacs-indentation-string            " ")
+  (treemacs-is-never-other-window         nil)
+  (treemacs-max-git-entries               5000)
+  (treemacs-missing-project-action        'ask)
+  (treemacs-move-forward-on-expand        nil)
+  (treemacs-no-png-images                 nil)
+  (treemacs-no-delete-other-windows       t)
+  (treemacs-project-follow-cleanup        nil)
+  (treemacs-position                      'left)
+  (treemacs-recenter-distance             0.1)
+  (treemacs-recenter-after-file-follow    nil)
+  (treemacs-recenter-after-tag-follow     nil)
+  (treemacs-recenter-after-project-jump   'always)
+  (treemacs-recenter-after-project-expand 'on-distance)
+  (treemacs-show-cursor                   nil)
+  (treemacs-show-hidden-files             t)
+  (treemacs-silent-filewatch              nil)
+  (treemacs-silent-refresh                nil)
+  (treemacs-sorting                       'alphabetic-asc)
+  (treemacs-space-between-root-nodes      t)
+  (treemacs-tag-follow-cleanup            t)
+  (treemacs-tag-follow-delay              1.5)
+  (treemacs-user-mode-line-format         nil)
+  (treemacs-user-header-line-format       nil)
+  (treemacs-width                         35)
   :config
-  (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-        treemacs-deferred-git-apply-delay      0.5
-        treemacs-directory-name-transformer    #'identity
-        treemacs-display-in-side-window        t
-        treemacs-eldoc-display                 t
-        treemacs-file-event-delay              5000
-        treemacs-file-extension-regex          treemacs-last-period-regex-value
-        treemacs-file-follow-delay             0.2
-        treemacs-file-name-transformer         #'identity
-        treemacs-follow-after-init             t
-        treemacs-git-command-pipe              ""
-        treemacs-goto-tag-strategy             'refetch-index
-        treemacs-indentation                   2
-        treemacs-indentation-string            " "
-        treemacs-is-never-other-window         nil
-        treemacs-max-git-entries               5000
-        treemacs-missing-project-action        'ask
-        treemacs-move-forward-on-expand        nil
-        treemacs-no-png-images                 nil
-        treemacs-no-delete-other-windows       t
-        treemacs-project-follow-cleanup        nil
-        treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-        treemacs-position                      'left
-        treemacs-recenter-distance             0.1
-        treemacs-recenter-after-file-follow    nil
-        treemacs-recenter-after-tag-follow     nil
-        treemacs-recenter-after-project-jump   'always
-        treemacs-recenter-after-project-expand 'on-distance
-        treemacs-show-cursor                   nil
-        treemacs-show-hidden-files             t
-        treemacs-silent-filewatch              nil
-        treemacs-silent-refresh                nil
-        treemacs-sorting                       'alphabetic-asc
-        treemacs-space-between-root-nodes      t
-        treemacs-tag-follow-cleanup            t
-        treemacs-tag-follow-delay              1.5
-        treemacs-user-mode-line-format         nil
-        treemacs-user-header-line-format       nil
-        treemacs-width                         35)
-
+  (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0))
+  (setq treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory))
   ;; The default width and height of the icons is 22 pixels. If you are
   ;; using a Hi-DPI display, uncomment this to double the icon size.
   ;;(treemacs-resize-icons 44)
