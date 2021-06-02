a2f9f356ffedc98178b2f5959289c82d34b41e5e
radixm46 <radixm46@gmail.com>
2021-06-02 17:30:58 +0900

    remove unnecessary lines

--- .emacs.d/elisp/modes.el
+++ .emacs.d/elisp/modes.el
@@ -107,8 +107,6 @@
     :ensure t
     :config (treemacs-icons-dired-mode))
 )
-
-
 ;(use-package treemacs-persp ;;treemacs-persective if you use perspective.el vs. persp-mode
 ;  :after treemacs persp-mode ;;or perspective vs. persp-mode
 ;  :ensure t
@@ -125,14 +123,6 @@
 (use-package rustic
   :ensure t
   :defer t
-  ;:init
-  ;(add-hook 'rustic-mode-hook #'lsp)
-  ;(add-hook 'rustic-mode-hook
-  ;  '(lambda ()
-  ;    (racer-mode t)
-  ;    (dumb-jump-mode t)
-  ;    (highlight-symbol-mode t)
-  ;    ))
   :hook
   ((rustic-mode . lsp)
    (rustic-mode . racer))
