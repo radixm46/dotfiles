878d32b49f11d52d314285126dd2d3549063de4f
radixm46 <radixm46@gmail.com>
2021-07-18 11:10:44 +0900

    add evil-surround
    and organize evil related modes

--- .emacs.d/elisp/minors.el
+++ .emacs.d/elisp/minors.el
@@ -131,19 +131,24 @@
   :ensure t
   :init (setq evil-want-keybinding nil)
   :custom (evil-undo-system 'undo-tree)
-  :config (evil-mode 1))
-(use-package evil-collection
-  :after evil
-  :ensure t
   :config
-  (evil-collection-init)
-  (define-key evil-normal-state-map (kbd "C-u") 'scroll-down-command)
-  ;; enable emacs cursor movement in insert mode
-  (define-key evil-insert-state-map (kbd "C-h") 'delete-backward-char)
-  (define-key evil-insert-state-map (kbd "C-f") 'forward-char)
-  (define-key evil-insert-state-map (kbd "C-b") 'backward-char)
-  ;(define-key evil-insert-state-map (kbd "C-p") 'previous-line)
-  ;(define-key evil-insert-state-map (kbd "C-n") 'next-line)
+  (use-package evil-surround
+    :ensure t
+    :config (global-evil-surround-mode t))
+  (use-package evil-collection
+    :after evil
+    :ensure t
+    :config
+    (evil-collection-init)
+    (define-key evil-normal-state-map (kbd "C-u") 'scroll-down-command)
+    ;; enable emacs cursor movement in insert mode
+    (define-key evil-insert-state-map (kbd "C-h") 'delete-backward-char)
+    (define-key evil-insert-state-map (kbd "C-f") 'forward-char)
+    (define-key evil-insert-state-map (kbd "C-b") 'backward-char)
+    ;; (define-key evil-insert-state-map (kbd "C-p") 'previous-line)
+    ;; (define-key evil-insert-state-map (kbd "C-n") 'next-line)
+    )
+  (evil-mode 1)
   )
 
 (use-package nyan-mode
