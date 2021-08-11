4cf47498a95f7152bae0397e209f9c7189ea5dc2
radixm46 <radixm46@gmail.com>
2021-08-11 11:49:41 +0900

    add leaf.el and straight to environment

--- .emacs.d/elisp/initpkg.el
+++ .emacs.d/elisp/initpkg.el
@@ -17,3 +17,23 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
+
+;; auto load leaf.el package
+(unless (package-installed-p 'leaf)
+  (package-refresh-contents)
+  (package-install 'leaf))
+(require 'leaf)
+
+;; setup straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
