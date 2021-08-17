bcb63f0c32c59351abaa7b366c6c5f6947985a50
radixm46 <radixm46@gmail.com>
2021-08-18 01:15:27 +0900

    reconstruct configuration with leaf
    move some package configurations to minors.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,50 +4,86 @@
 ;; You may delete these explanatory comments.
 
 ;; emacs configuration by radixM491VA
-(setq inhibit-splash-screen t)
-(column-number-mode t)
-(setq ring-bell-function 'ignore)
-(setq-default truncate-lines t)
-;; (global-visual-line-mode nil)
-;; (setq line-move-visual t)
-;; (setq word-wrap t)
-(setq create-lockfiles nil)
 
-;; check directory and set target
-(let ((my-autosave-dir "~/.emacs.d/.cache/autosaved")
-      (my-hist-dir "~/.emacs.d/.cache/hist"))
-  (if (not (file-directory-p my-autosave-dir))
-      (make-directory my-autosave-dir t))
-  (if (not (file-directory-p my-hist-dir))
-      (make-directory my-hist-dir t)))
+;; ---------------  load package ---------------
+(load "~/.emacs.d/elisp/initpkg.el")
 
-;; configure auto save files
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/.cache/autosaved" t)))
-(setq delete-auto-save-files t)
-(setq auto-save-default t)
-(setq auto-save-timeout 15)
-(setq auto-save-interval 120)
-(setq auto-save-list-file-prefix nil)
+(leaf set-appearance
+  :config
+  (setq inhibit-splash-screen t)
+  (column-number-mode t)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (blink-cursor-mode 0)
+  (scroll-bar-mode -1)
+  (display-time)
+  (which-function-mode 1)
+  (transient-mark-mode 1)
+  (setq ring-bell-function 'ignore)
+  ;; (global-visual-line-mode nil)
+  ;; (setq line-move-visual t)
+  ;; (setq word-wrap t)
+  (setq default-frame-alist (append (list '(font . "HackGen35Nerd Console-14.1")
+                                          '(width . 80)
+                                          '(height . 55)) default-frame-alist))
+  (setq font-for-tables "HackGenNerd Console")
 
-;; make backup files
-(setq backup-directory-alist '((".*" .  "~/.emacs.d/.cache/hist")))
-(setq make-backup-files t)
-(setq version-control t) ;; enable version control
-(setq kept-new-versions 5)
-(setq kept-old-versions 1)
-(setq delete-old-versions t)
+  (leaf set-linespacing
+    :config
+    ;; control line spacing
+    (setq-default line-spacing 0.25)
+    (defun sw-lnsp (wdth)
+      "switch line spacing"
+      (interactive "nset line spacing: ")
+      (setq line-spacing wdth))
+    (defun dbl-lnsp ()
+      "switch line spacing to double"
+      (interactive)
+      (setq line-spacing 2.0))
+    )
+  )
 
-(setq default-frame-alist (append (list '(font . "HackGen35Nerd Console-14.1")
-                                        '(width . 80)
-                                        '(height . 55)) default-frame-alist))
-(setq font-for-tables "HackGenNerd Console")
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(blink-cursor-mode 0)
-(scroll-bar-mode -1)
-(display-time)
+(leaf conf-cache-history
+  :config
+  (leaf set-cache-path
+    :doc "check directory and set target"
+    :config
+    (let ((my-autosave-dir "~/.emacs.d/.cache/autosaved")
+          (my-hist-dir "~/.emacs.d/.cache/hist"))
+      (if (not (file-directory-p my-autosave-dir))
+          (make-directory my-autosave-dir t))
+      (if (not (file-directory-p my-hist-dir))
+          (make-directory my-hist-dir t)))
+    )
 
-(define-key global-map [f9] 'other-frame)
+  (leaf autosave
+    :doc "configure auto save files"
+    :config
+    (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/.cache/autosaved" t)))
+    (setq delete-auto-save-files t)
+    (setq auto-save-default t)
+    (setq auto-save-timeout 15)
+    (setq auto-save-interval 120)
+    (setq auto-save-list-file-prefix nil))
+
+  (leaf backup-files
+    :doc "make backup files"
+    :config
+    (setq backup-directory-alist '((".*" .  "~/.emacs.d/.cache/hist")))
+    (setq make-backup-files t)
+    (setq version-control t) ;; enable version control
+    (setq kept-new-versions 5)
+    (setq kept-old-versions 1)
+    (setq delete-old-versions t)
+    (setq create-lockfiles nil))
+
+  (leaf history
+    :doc "configure history, log"
+    :config
+    (setq message-log-maxa 10000)
+    (setq history-length 1000)
+    (setq history-delete-duplicates t))
+  )
 
 ;; mouse support on terminal
 (unless window-system
@@ -55,66 +91,41 @@
   (xterm-mouse-mode t)
   (defun track-mouse (e))
   (setq mouse-sel-mode t))
+(defun emacs-works-on-term-p ()
+  "returns t if emacs seems running on term"
+  (not (or (daemonp) (display-graphic-p))))
+(define-key global-map [f9] 'other-frame)
 
 
-;; control line spacing
-(setq-default line-spacing 0.25)
+(leaf lang-locales
+  :config
+  ;; locale and encoding
+  (set-locale-environment nil)
+  (set-default 'buffer-file-coding-system 'utf-8)
+  (set-language-environment "Japanese")
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (setq default-coding-systems 'utf-8)
+  ;; (setq coding-system-for-read 'utf-8) ;; conflicts ddskk
+  (setq coding-system-for-write 'utf-8)
+  )
 
-(defun sw-lnsp (wdth)
-  (interactive "nset line spacing: ")
-  (setq line-spacing wdth))
-(defun dbl-lnsp ()
-  (interactive)
-  (setq line-spacing 2.0))
-
-  (defun emacs-works-on-term-p ()
-    "returns t if emacs seems running on term"
-      (not (or (daemonp) (display-graphic-p))))
-
-;; log
-(setq message-log-maxa 10000)
-(setq history-length 1000)
-(setq history-delete-duplicates t)
-
-;; locale and encoding
-(set-locale-environment nil)
-(set-default 'buffer-file-coding-system 'utf-8)
-(set-language-environment "Japanese")
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(setq default-coding-systems 'utf-8)
-;; (setq coding-system-for-read 'utf-8) ;; conflicts ddskk
-(setq coding-system-for-write 'utf-8)
-
-;; configure indent tab to false as default
-(setq-default tab-width 4 indent-tabs-mode nil)
-
-(setq eol-mnemonic-dos "(CRLF)")
-(setq eol-mnemonic-mac "(CR)")
-(setq eol-mnemonic-unix "(LF)")
-(which-function-mode 1)
-(transient-mark-mode 1)
+(leaf editor-defaults
+  :config
+  ;; configure indent tab to false as default
+  (setq-default truncate-lines t)
+  (setq-default tab-width 4 indent-tabs-mode nil)
+  (setq eol-mnemonic-dos "(CRLF)")
+  (setq eol-mnemonic-mac "(CR)")
+  (setq eol-mnemonic-unix "(LF)")
+  )
 
 (setq find-file-visit-truename t)
 
-
-;; ---------------  load package ---------------
-(load "~/.emacs.d/elisp/initpkg.el")
-
 ;; -------- package config under use-package --------
-(use-package exec-path-from-shell
-  :ensure t
-  :init
-  (setq exec-path-from-shell-arguments (list "-l"))
-  :config
-  (when (daemonp)
-    (exec-path-from-shell-initialize)))
-
-(use-package all-the-icons
-  :ensure t) ;; need installation by all-the-icons-install-fonts
 
 ;; --------------- minor modes ---------------
 (load "~/.emacs.d/elisp/minors.el")
