8f985f85becc63cedc3413e65c750347901e274c
radixm46 <radixm46@gmail.com>
2021-08-21 13:33:47 +0900

    move leaf to init.el
    - evil
    - quickrun

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -368,6 +368,32 @@
     :ensure t
     :hook (prog-mode-hook . yas-minor-mode)
     :config (yas-reload-all))
+
+  (leaf evil
+    :ensure t
+    :init (setq evil-want-keybinding nil)
+    :custom (evil-undo-system . 'undo-tree)
+    :config
+    (leaf evil-surround
+      :ensure t
+      :global-minor-mode global-evil-surround-mode)
+    (leaf evil-collection
+      :after evil
+      :ensure t
+      :config
+      (evil-collection-init)
+      (define-key evil-normal-state-map (kbd "C-u") 'scroll-down-command)
+      ;; enable emacs cursor movement in insert mode
+      (define-key evil-insert-state-map (kbd "C-h") 'delete-backward-char)
+      (define-key evil-insert-state-map (kbd "C-f") 'forward-char)
+      (define-key evil-insert-state-map (kbd "C-b") 'backward-char)
+      ;; (define-key evil-insert-state-map (kbd "C-p") 'previous-line)
+      ;; (define-key evil-insert-state-map (kbd "C-n") 'next-line)
+      )
+    :global-minor-mode evil-mode
+    )
+
+  (leaf quickrun :ensure t)
   )
 
 (leaf ido
@@ -508,8 +534,7 @@
     :ensure t
     :custom (magit-completing-read-function . 'magit-ido-completing-read))
   )
-;; --------------- minor modes ---------------
-(load "~/.emacs.d/elisp/minors.el")
+
 
 ;; --------------- major modes ---------------
 (load "~/.emacs.d/elisp/majors-s.el")
