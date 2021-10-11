865ee4e9fed3a576126454a5c3fa2e27182b36ec
radixm46 <radixm46@gmail.com>
2021-10-11 09:26:34 +0900

    fix hook
    - fix name of hook

--- .emacs.d/elisp/langs.el
+++ .emacs.d/elisp/langs.el
@@ -147,7 +147,7 @@
 (leaf typescript-mode
   :ensure t
   :mode ("\\.ts\\'" . typescript-mode)
-  :hook (typescript-mode . lsp)
+  :hook (typescript-mode-hook . lsp)
   :custom
   (typescript-indent-level . 2)
   :config
