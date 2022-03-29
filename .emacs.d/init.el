f422bb0d78eae5bd85192b3f3454eb05e6fc4b94
radixm46 <radixm46@gmail.com>
2022-03-29 20:42:29 +0900

    clean multiple evil-define-key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1396,15 +1396,16 @@
     (neo-window-position   . 'right)
     :config
     ;; keymap for using with evil mode
-    (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
-    (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
-    (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
-    (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
-    (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
-    (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
-    (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
-    (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
-    (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle))
+    (evil-define-key 'normal neotree-mode-map
+      (kbd "TAB") 'neotree-enter
+      (kbd "SPC") 'neotree-quick-look
+      (kbd "q")   'neotree-hide
+      (kbd "RET") 'neotree-enter
+      (kbd "g")   'neotree-refresh
+      (kbd "n")   'neotree-next-line
+      (kbd "p")   'neotree-previous-line
+      (kbd "A")   'neotree-stretch-toggle
+      (kbd "H")   'neotree-hidden-file-toggle))
 
   (leaf projectile
     :ensure t
