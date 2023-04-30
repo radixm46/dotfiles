df546f41dcd2f2874527bfb2e036217ed415d285
radixm46 <radixm46@gmail.com>
2023-05-01 06:10:33 +0900

    improve screen capturing

---
+++ bin/screencap.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# dependency check
+for cmd in grim slurp wl-copy notify-send; do
+    if ! command -v "${cmd}" > /dev/null 2>&1; then
+        echo "Error: ${cmd} is not installed."
+        exit 1
+    fi
+done
+
+# targetdir
+dir="${HOME}/Pictures/Screenshots"
+[ ! -d "${dir}" ] && mkdir -p "${dir}"
+
+# options
+trim=false
+clipboard=false
+while getopts "tc" opt; do
+    case "${opt}" in
+        t) trim=true ;;
+        c) clipboard=true ;;
+        *) exit 1 ;;
+    esac
+done
+
+# trim
+if $trim; then
+    geometry=$(slurp) || exit 1 # if geometry not acquired, quit
+else
+    geometry=""
+fi
+
+# handle screenshot data
+if ${clipboard}; then
+    # configure temp file for clipboard image
+    TMPDIR="${HOME}/.local/tmp"
+    [ ! -d "${TMPDIR}" ] && mkdir -p "${TMPDIR}"
+    trap 'rm -f "${tmpfile}"' EXIT
+
+    if [ -z "${geometry}" ]; then
+        cap_copy() {
+            grim - | wl-copy
+        }
+        cap_copy || exit 1
+    else
+        cap_copy_trim() {
+            grim -g "$geometry" - | wl-copy
+        }
+        cap_copy_trim || exit 1
+    fi
+    tmpfile=$(mktemp -p "${TMPDIR}")
+    wl-paste > "${tmpfile}"
+    notify-send "Image captured!" "Copied to clipboard! 📋" \
+                --urgency="low" --category="screenshot" \
+                --hint=string:image-path:"${tmpfile}"
+                # --hint=string:onclick-action:"imv ${tmpfile}" # これよくわからん、というか効いてない感じするな
+else
+    imgfile="grim $(date '+%Y-%m-%d %H.%M.%S').png"
+    imgpath="${dir}/${imgfile}"
+    if [ -z "${geometry}" ]; then
+        grim "${imgpath}" || exit 1
+    else
+        grim -g "${geometry}" "${imgpath}" || exit 1
+    fi
+    notify-send "Image saved!" "${imgpath}" \
+                --urgency="low" --category="screenshot" \
+                --hint=string:image-path:"${imgpath}"
+fi
