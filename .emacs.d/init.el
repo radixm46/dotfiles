7f7242ca2892cfb1b1e98a9b15add74398b633b6
radixm46 <radixm46@adms-MacBook-Air.local>
2023-05-10 17:38:05 +0900

    modify evil-cleverparens keymap

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -467,7 +467,13 @@ If no font in `fonts' matches and `func-fail' is given, invoke `func-fail'.
     (leaf evil-cleverparens
       :doc "edit for lispy modes"
       :ensure t
-      :hook (smartparens-enabled-hook . evil-cleverparens-mode))
+      :hook (smartparens-enabled-hook . evil-cleverparens-mode)
+      :config
+      (evil-define-key 'normal evil-cleverparens-mode-map
+        "[" 'evil-cp-previous-opening
+        "]" 'evil-cp-next-opening
+        "{" 'evil-cp-previous-closing
+        "}" 'evil-cp-next-closing))
     )
 
   (leaf editorconfig
