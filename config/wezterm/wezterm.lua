06448a877399de4d6ee47e48975db103f64af246
radixm46 <radixm46@gmail.com>
2023-11-10 09:56:28 +0900

    place tab bar to top

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -52,7 +52,7 @@ do
    ---- define tab style ----
    -- use customized classic style tab bar
    config.use_fancy_tab_bar            = false
-   config.tab_bar_at_bottom            = true
+   config.tab_bar_at_bottom            = false
    config.hide_tab_bar_if_only_one_tab = true
    config.tab_max_width                = 50 -- for longer tab name
 
