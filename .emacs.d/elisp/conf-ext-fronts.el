b484c135ef47e333029a984777d1f9428852a760
radixm46 <radixm46@gmail.com>
2024-10-09 15:16:15 +0900

    add mastodon

--- .emacs.d/elisp/conf-ext-fronts.el
+++ .emacs.d/elisp/conf-ext-fronts.el
@@ -6,7 +6,8 @@
 ;;; Code:
 (eval-when-compile
   (load (expand-file-name "elisp/initpkg" user-emacs-directory))
-  (!el-load "elisp/doom"))
+  (!el-load "elisp/doom"
+            "elisp/conf-evil"))
 (require 'straight)
 
 (leaf rg :if (!executable-find "rg")
@@ -106,4 +107,84 @@
     (transient-append-suffix 'magit-diff '(-1 -1)
       [("D" "Difftastic diff (dwim)" difftastic-magit-diff)
        ("S" "Difftastic show" difftastic-magit-show)])))
+(leaf mastodon
+  :preface
+  (leaf emojify
+    :ensure t
+    :custom `((emojify-emojis-dir . ,(cache-sub-dir "emojify"))))
+  :ensure t
+  :custom
+  (mastodon-media--enable-image-caching . t)
+  :init (when (fboundp 'evil-lion-mode) (evil-lion-mode nil))
+  :commands mastodon mastodon-toot
+  :defvar
+  (mastodon-mode-map mastodon-toot-mode-map
+   mastodon-profile-mode-map mastodon-search-mode-map)
+  :config
+  ;; Timeline Mode Keybindings
+  (evil-define-key 'normal mastodon-mode-map
+    (kbd "C-j") 'evil-next-line
+    (kbd "C-k") 'evil-previous-line
+    (kbd "j") 'mastodon-tl--goto-next-item
+    (kbd "k") 'mastodon-tl--goto-prev-item
+    ;; operate on toot
+    (kbd "t") 'mastodon-toot
+    (kbd "e") 'mastodon-toot--edit-toot-at-point
+    (kbd "r") 'mastodon-toot--reply
+    (kbd "d") 'mastodon-toot--delete-toot
+    (kbd "y") 'mastodon-toot--copy-toot-url
+    (kbd "o") 'mastodon-toot--open-toot-url
+    (kbd "f") 'mastodon-toot--toggle-favourite
+    (kbd "b") 'mastodon-toot--toggle-boost
+    (kbd "B") 'mastodon-toot--toggle-bookmark
+    (kbd "P") 'mastodon-toot--pin-toot-toggle
+    ;; view
+    (kbd "s") 'mastodon-tl--toggle-spoiler-text-in-toot
+    (kbd "v") 'mastodon-tl--poll-vote
+    (kbd "/") 'mastodon-search--query
+    ;; operate on user
+    (kbd "u p") 'mastodon-profile--show-user
+    (kbd "u d") 'mastodon-tl--dm-user
+    (kbd "u f") 'mastodon-tl--follow-user
+    (kbd "u F") 'mastodon-tl--unfollow-user
+    (kbd "u M") 'mastodon-tl--mute-user
+    (kbd "u m") 'mastodon-tl--unmute-user
+    (kbd "u B") 'mastodon-tl--block-user
+    (kbd "u b") 'mastodon-tl--unblock-user
+    ;; timeline, lists
+    (kbd "g r") 'mastodon-tl--update
+    (kbd "g h") 'mastodon-tl--get-home-timeline
+    (kbd "g L") 'mastodon-tl--get-local-timeline
+    (kbd "g S") 'mastodon-tl--get-federated-timeline
+    (kbd "g #") 'mastodon-tl--get-tag-timeline
+    (kbd "g n") 'mastodon-notifications-get
+    (kbd "g m") 'mastodon-notifications--get-mentions
+    (kbd "g l") 'mastodon-views--view-lists
+    (kbd "g f") 'mastodon-views--view-follow-suggestions
+    (kbd "g p") 'mastodon-profile--my-profile
+    (kbd "g b") 'mastodon-profile--view-bookmarks
+    (kbd "g v") 'mastodon-profile--view-favourites
+    ;; etc
+    (kbd "q") 'mastodon-kill-window
+    (kbd "Q") 'mastodon-kill-all-buffers
+    ;; Leader key mappings
+    (kbd "z r") 'mastodon-tl--report-to-mods
+    (kbd "z z") 'mastodon-views--view-instance-description
+    )
+
+  ;; Mastodon Profile Mode
+  (evil-define-key 'normal mastodon-profile-mode-map
+    (kbd "TAB") 'mastodon-profile--account-view-cycle
+    (kbd "/") 'mastodon-profile--account-search
+    (kbd ", #") 'mastodon-profile--open-statuses-tagged
+    )
+
+  ;; Mastodon Search Mode
+  (evil-define-key 'normal mastodon-search-mode-map
+    (kbd "n") 'mastodon-search--next-result
+    (kbd "N") 'mastodon-search--previous-result
+    (kbd "TAB") 'mastodon-search--query-cycle
+    (kbd "/") 'mastodon-search--query
+    )
+  )
 ;;; conf-ext-fronts.el ends here
