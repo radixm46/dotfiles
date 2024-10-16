cc67b5a92577813de1256d77ce55a8b64caf8aea
radixm46 <radixm46@gmail.com>
2024-10-16 20:03:32 +0900

    set shr image size

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2200,6 +2200,7 @@ Enforce a sneaky Garbage Collection strategy to minimize GC interference with us
     `((shr-width                   . 85)
       (shr-indentation             . 4)
       (shr-use-fonts               . t)
+      (shr-max-image-proportion    . 0.6)
       (url-configuration-directory . ,(cache-sub-dir "url"))
       (eww-bookmarks-directory     . ,(cache-sub-dir "eww"))
       (eww-search-prefix           . "https://www.google.com/search?q=")
